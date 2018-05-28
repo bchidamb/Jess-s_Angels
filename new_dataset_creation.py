654b1aed5f65b313745ae21eb5929625dedff3c8
Connor Soohoo <connorsoohoo@gmail.com>
2018-05-27 17:53:46 -0700

    modified training and probe sets

--- new_dataset_creation.py
+++ new_dataset_creation.py
@@ -23,7 +23,7 @@ real_probe_indices = np.random.choice(four, probe_size, replace=False)
 probe_set = mu_data.loc[real_probe_indices].copy()
 
 probe_set.to_csv("data/real_mu_probe.csv")
-probe_set["Rating"].to_csv("data/real_mu_probe.txt", header=None, index=False, sep=' ')
+probe_set.drop(["Date Number"], axis=1).to_csv("data/real_mu_probe.txt", header=None, index=False, sep=' ')
 print(len(probe_set))
 
 one = mu_idx.loc[mu_idx["Index"] == 1]
@@ -37,7 +37,7 @@ train_list += list(set(four) - set(real_probe_indices))
 
 mu_train = mu_data.loc[train_list]
 mu_train.to_csv("data/real_mu_train.csv")
-mu_train["Rating"].to_csv("data/real_mu_train.txt", header=None, index=False, sep=' ')
+mu_train.drop(["Date Number"], axis=1).to_csv("data/real_mu_train.txt", header=None, index=False, sep=' ')
 print(len(mu_train))
 
 
@@ -60,7 +60,7 @@ real_probe_indices = np.random.choice(four, probe_size, replace=False)
 probe_set = um_data.loc[real_probe_indices].copy()
 
 probe_set.to_csv("data/real_um_probe.csv")
-probe_set["Rating"].to_csv("data/real_um_probe.txt", header=None, index=False, sep=' ')
+probe_set.drop(["Date Number"], axis=1).to_csv("data/real_um_probe.txt", header=None, index=False, sep=' ')
 
 print(len(probe_set))
 
@@ -75,5 +75,5 @@ train_list += list(set(four) - set(real_probe_indices))
 
 um_train = um_data.loc[train_list]
 um_train.to_csv("data/real_um_train.csv")
-um_train["Rating"].to_csv("data/real_um_train.txt", header=None, index=False, sep=' ')
+um_train.drop(["Date Number"], axis=1).to_csv("data/real_um_train.txt", header=None, index=False, sep=' ')
 print(len(um_train))
